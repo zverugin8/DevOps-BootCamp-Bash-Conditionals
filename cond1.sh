e648d2370c2f0ce95ad1b711091911e6d1fd990c
Siarhei Saroka <siarhei_saroka@epam.com>
2022-12-14 22:02:10 +0300

    task1 done

--- cond1.sh
+++ cond1.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 IFS=',' read -r -a array <<< "$1"
+declare -i res=0
+for val in "${array[@]}"; do
 
-#Write your code here
+  if (( val%2==0 )); then 
+    ((res+=val))
+  fi
+done
+echo "$res"
+
