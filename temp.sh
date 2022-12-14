e0d7d224076a807ed0eb6b03f6b37d4344b29873
Siarhei Saroka <siarhei_saroka@epam.com>
2022-12-14 22:26:37 +0300

    Task2 Done

--- temp.sh
+++ temp.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-# Place your code here
+if [[ -z $1 ]]; then echo "Reqired input temperature as arg1" ; exit 1 ; fi
+
+arg1=$1
+res=0
+if [[ ${arg1:((${#arg1}-1)):1} = "C" ]]; then
+    ((res=${arg1:0:((${#arg1}-1))}+273))
+    echo $res"K"
+elif [[ ${arg1:((${#arg1}-1)):1} = "K" ]]; then
+    ((res=${arg1:0:((${#arg1}-1))}-273))
+     echo $res"C"
+fi
+
+
